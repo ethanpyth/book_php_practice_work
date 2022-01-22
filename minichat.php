0f8734a18081d9dba1b7a880486506f8940cdac0
Excellence KAWEJ <19ka201@esisalama.org>
2022-01-22 23:16:53 +0200

    fin du projet

--- minichat.php
+++ minichat.php
@@ -4,21 +4,31 @@
     <meta charset="UTF-8">
     <title>MINI CHAT</title>
 </head>
+<style>
+    form{
+        text-align: center;
+    }
+</style>
 <body>
-    <div>
-        <form method="post" action="minichatpost.php">
-            Pseudo: <input type="text" name="pseudo" placeholder="Entrer votre pseudo"> <br>
-        </form>
-    </div>
-    <div>
-        <?php
-        ?>
-    </div>
-    <div>
-        <form method="post" action="minichat.php">
-            Message : <input type="textarea" name="message" placeholder="Entrer votre message">
+    <form method="post" action="minichatpost.php">
+        <p>
+            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" placeholder="Entrer votre pseudo"><br>
+            <label for="message">Message</label> : <input type="textarea" name="message" placeholder="Entrer votre message">
             <input type="submit">
-        </form>
-    </div>
+        </p>
+    </form>
+    <?php
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
+    }catch (Exception $e){
+        die('Erreur :' . $e->getMessage());
+    }
+    $reponse = $bdd->query('SELECT * FROM minichat ORDER BY id limit 0, 10');
+    while ($donnees = $reponse->fetch())
+    {
+        echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> :' . htmlspecialchars($donnees['message']) . '</p>';
+    }
+    $reponse->closeCursor();
+    ?>
 </body>
 </html>
--- minichatpost.php
+++ minichatpost.php
@@ -1,16 +1,17 @@
 <?php
     //Effectuer ici la requette qui insère le message
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
-}catch (Exception $e){
-    die('Erreur :' . $e->getMessage());
-}
-$req = $bdd->exec('INSERT INTO minichat(pseudo, message) VALUES (:pseudo, :message)');
-$req->execute(array(
-    'nom' => $_POST['pseudo'],
-    'message' => $_POST['message']
-));
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
+    }catch (Exception $e){
+        die('Erreur :' . $e->getMessage());
+    }
+    $req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES (?, ?)');
+    $req->execute(array(
+        $_POST['pseudo'],
+        $_POST['message']
+    ));
 
     //Puis rediriger vers minichat.php comme ceci :
     header('Location: minichat.php');
+    $req->closeCursor();
 ?>
