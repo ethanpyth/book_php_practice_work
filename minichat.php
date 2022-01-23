12ff74cc1f9257f6a15b6541e3c89dce77210186
Excellence KAWEJ <19ka201@esisalama.org>
2022-01-23 11:47:42 +0200

    stylisation du minichat

--- minichat.php
+++ minichat.php
@@ -4,31 +4,29 @@
     <meta charset="UTF-8">
     <title>MINI CHAT</title>
 </head>
-<style>
-    form{
-        text-align: center;
-    }
-</style>
 <body>
+    <link href="minichat.css">
     <form method="post" action="minichatpost.php">
         <p>
-            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" value="<?php echo $_COOKIE['pseudo'] ?>" placeholder="Entrer votre pseudo"><br>
+            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" value="<?php echo htmlspecialchars($_COOKIE['pseudo']) ?>" placeholder="Entrer votre pseudo"><br>
             <label for="message">Message</label> : <input type="textarea" name="message" placeholder="Entrer votre message">
             <input type="submit">
         </p>
     </form>
-    <?php
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
-    }catch (Exception $e){
-        die('Erreur :' . $e->getMessage());
-    }
-    $reponse = $bdd->query('SELECT * FROM minichat ORDER BY id limit 0, 10');
-    while ($donnees = $reponse->fetch())
-    {
-        echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> :' . htmlspecialchars($donnees['message']) . '</p>';
-    }
-    $reponse->closeCursor();
-    ?>
+    <div id="message_zone">
+        <?php
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
+        }catch (Exception $e){
+            die('Erreur :' . $e->getMessage());
+        }
+        $reponse = $bdd->query('SELECT * FROM minichat ORDER BY id limit 0, 10');
+        while ($donnees = $reponse->fetch())
+        {
+            echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+        }
+        $reponse->closeCursor();
+        ?>
+    </div>
 </body>
 </html>
